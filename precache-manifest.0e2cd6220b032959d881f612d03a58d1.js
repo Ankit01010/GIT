1ddf32b57d89ab3ae00e63261d53d7c35e6384ff
Ankit01010 <sunnysingh10099@gmail.com>
2022-04-12 20:13:12 +0530

    Updates

---
+++ precache-manifest.0e2cd6220b032959d881f612d03a58d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8012e5ca834f6926a5a",
+    "url": "/Text-Utils/static/css/main.d782b8e0.chunk.css"
+  },
+  {
+    "revision": "a8012e5ca834f6926a5a",
+    "url": "/Text-Utils/static/js/main.a8012e5c.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/Text-Utils/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "ddfca05d5e3b04e75453",
+    "url": "/Text-Utils/static/js/2.ddfca05d.chunk.js"
+  },
+  {
+    "revision": "d9467d276c2c9b89a881",
+    "url": "/Text-Utils/static/js/runtime~main.d9467d27.js"
+  },
+  {
+    "revision": "0b15805995854bea43391f8dfd800a6b",
+    "url": "/Text-Utils/index.html"
+  }
+];
