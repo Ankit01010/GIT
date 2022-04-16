83876688031fac91f3ccc5deeb3f2d8973c00f68
Ankit01010 <97511749+Ankit01010@users.noreply.github.com>
2022-04-16 14:49:38 +0530

    Add files via upload

---
+++ build/precache-manifest.3fa26be83c6adda218718c3901eb88c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe48092445945fa4c61a",
+    "url": "/Text-Utils/static/css/main.d782b8e0.chunk.css"
+  },
+  {
+    "revision": "fe48092445945fa4c61a",
+    "url": "/Text-Utils/static/js/main.fe480924.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/Text-Utils/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "6204e49779d177750627",
+    "url": "/Text-Utils/static/js/2.6204e497.chunk.js"
+  },
+  {
+    "revision": "d9467d276c2c9b89a881",
+    "url": "/Text-Utils/static/js/runtime~main.d9467d27.js"
+  },
+  {
+    "revision": "7176d6ee7ad6379d966f1109924f2586",
+    "url": "/Text-Utils/index.html"
+  }
+];
